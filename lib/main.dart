2b09ebf0d30d1580a2971eb78a63c8096b9e3478
Noel George C J <noelgeorge2004@gmail.com>
2023-10-21 00:09:38 +0530

    Opening screen created

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        fontFamily: 'Klasik'
       ),
       home: SplashStart()
     );
--- lib/pages/splash_start.dart
+++ lib/pages/splash_start.dart
@@ -7,11 +7,21 @@ class SplashStart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        padding: EdgeInsets.only(top: 70),
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/splash_1.png'),
             fit: BoxFit.fill
           ),
+        ),
+        child: const Text(
+          "WELCOME TO MONUMENTAL HABITS",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 40,
+          )
         ),
       )
     );
