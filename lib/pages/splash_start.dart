222ff2b59e9d7359221a4571ad696b8d9dec0686
Noel George C J <noelgeorge2004@gmail.com>
2023-10-20 23:49:41 +0530

    initial commit

---
+++ lib/pages/splash_start.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashStart extends StatelessWidget {
+  const SplashStart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/splash_1.png'),
+            fit: BoxFit.fill
+          ),
+          color: Colors.amber
+        ),
+      )
+    );
+  }
+}
